a36ba19363bbae5949d0083dc8b743391dfbb8a4
idenma <idenma2@gmail.com>
2022-09-26 19:33:05 +0900

    Completed for now 20220926

--- picrepeat.rb
+++ picrepeat.rb
@@ -16,7 +16,13 @@ newimg_height = image.rows * height
 
 newimg = Image.new(newimg_width, newimg_height)
 
-newimg.composite!(image,0,0,OverCompositeOp)
+(0..image.rows - 1).each do |y|
+  (0..image.columns - 1).each do |x|
+
+    newimg.composite!(image,x * image.columns,y * image.rows,OverCompositeOp)
+  end
+end
+
 
 dt = Date.today
 newimg.write("./result/" + dt.year.to_s + dt.month.to_s + dt.day.to_s + ('a'..'Z').to_a.shuffle[0..7].join + ".png")
